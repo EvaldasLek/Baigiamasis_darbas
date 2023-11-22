d19e0d4da29fd962e10581ac43eb3ef511ac6043
MartynasVCS <martynasvcs@gmail.com>
2023-11-22 09:42:54 +0200

    Add demo test

--- Tests/Class1.cs
+++ Tests/Class1.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Framework;
+using Framework.Pages;
+using NUnit.Framework;
 
 namespace Tests
 {
     public class Class1
     {
+        [Test]
+        public void Demo()
+        {
+            Driver.InitializeDriver();
+            Home.Open();
+            Home.CloseNewsletter();
+            Home.CloseNewsletterMinimized();
+            Home.CloseCookies();
+        }
     }
 }
